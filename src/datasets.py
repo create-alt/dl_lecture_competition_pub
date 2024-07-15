7a822e0e71e0a6ceb1c6a99e57853ae9c3326efd
create-alt <134464021+create-alt@users.noreply.github.com>
2024-07-15 19:06:08 +0900

    Update datasets.py

--- src/datasets.py
+++ src/datasets.py
@@ -22,7 +22,7 @@ from src.utils import RepresentationType, VoxelGrid, flow_16bit_to_float
 
 VISU_INDEX = 1
 
-transform = tf.Compose([tf.RandomCrop(32, padding=(30, 30, 40, 40), padding_mode='constant'),  # random cropping
+transform = tf.Compose([tf.RandomCrop(32, padding=(15, 15, 20, 20), padding_mode='constant'),  # random cropping
                                 tf.ToTensor()])
 
 class EventSlicer:
